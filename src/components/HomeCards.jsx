36e035653ef184d50332706be0a431d44c41844c
Adam Wright <adamdenverco@gmail.com>
2025-01-07 11:09:30 -0600

    save

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -8,7 +8,6 @@ const HomeCards = ({ children }) => {
     <section className="py-4">
         <div className="container-xl lg:container m-auto">
 
-
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
 
                 {/* https://www.youtube.com/watch?v=LDB4uaJ87e0&t=154s

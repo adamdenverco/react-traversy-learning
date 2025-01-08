7e61d411e03365617669388b90a59bd292ccc774
Adam Wright <32560955+adamdenverco@users.noreply.github.com>
2025-01-07 21:39:55 -0600

    2024-01-07 traversy react work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Roboto', 'sans-serif']
-      }
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
     },
-    gridTemplateColumns: {
-      '70/30': '70% 28%'
-    }
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
